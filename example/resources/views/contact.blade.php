e8378323bf1103387e5cd232cef9f27256ce0946
5poyro <205345@student.aventus.nl>
2025-06-05 11:42:17 +0200

    Laravel
    Mijn laravelproject voor github

---
+++ example/resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+     <x-slot:header>
+        Contact
+    </x-slot:header>
+    <h1> This is Contact LMAO </h1>
+</x-layout>
+
---
+++ example/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:header>
+        Homepage
+    </x-slot:header>
+
+
+</x-layout> 
+
